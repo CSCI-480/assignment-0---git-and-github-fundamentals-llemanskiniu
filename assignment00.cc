8a386f5cd67629347ef65ca3ec474f718da6c676
llemanskiniu <Z1885842@students.niu.edu>
2022-01-27 10:07:29 -0600

    First

---
+++ assignment00.cc
@@ -0,0 +1,9 @@
+/*
+    Luke Lemanski
+    Z1885842
+    CSCI-480-PE1
+
+I certify that this is my own work and where appropriate an
+ extension of the starter code provided for the assignment.
+
+*/
